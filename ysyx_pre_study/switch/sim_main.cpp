d716204971e102e7285987eaaf0dbfdcd595c0cc
Ruihong <sgryin2@gmail.com>
2022-08-10 05:30:19 -0400

    can not generate vcd file

--- ysyx_pre_study/switch/sim_main.cpp
+++ ysyx_pre_study/switch/sim_main.cpp
@@ -2,18 +2,34 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "Vswitch.h"
+#include <verilated_vcd_c.h>
 
-int main(int argc, char** argv){
+vluint64_t sc_time = 0;
+double sc_time_stamp()
+{
+		return sc_time;
+}
+int main(int argc, char** argv, char** env){
 	VerilatedContext* contextp = new VerilatedContext;
 	contextp->commandArgs(argc,argv);
+	// vcd
+	Verilated::traceEverOn(true);
+	VerilatedVcdC* m_trace = new VerilatedVcdC;
 	Vswitch* top = new Vswitch{contextp};
-	while(!contextp->gotFinish()){
+	top->trace(m_trace,0);
+	m_trace->open("waveform.vcd");
+	while(sc_time_stamp() < 20 && !contextp->gotFinish()){
 		int a = rand() & 1;
 		int b = rand() & 1;
 		top->a = a;
 		top->b = b;
 		top->eval();
 		printf("a=%d, b = %d, f = %d\n", a, b, top->f);
-		assert(top->f == a ^ b); 
-	}
+		assert(top->f == a ^ b);
+		m_trace->dump(sc_time);
+		sc_time++;
+	} 
+	m_trace->close();
+	delete top;
+	return 0;
 }
